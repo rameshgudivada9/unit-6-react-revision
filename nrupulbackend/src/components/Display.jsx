9d9071d5e547efcae61921c2a4c34fb62b8a0f90
rameshgudivada9 <96101948+rameshgudivada9@users.noreply.github.com>
2022-06-07 00:28:34 +0530

    changes

--- nrupulbackend/src/components/Display.jsx
+++ nrupulbackend/src/components/Display.jsx
@@ -5,22 +5,21 @@ export const Display=({props})=>{
     console.log(props)
 
     return(
-        
+        <div>
         <div className="main">
     {props.map((el,i)=>{
         return (
             <div key={i} >
-                <div><img src={el.img}/></div>
-                <div>{el.name}</div>
-                <div>{el.colorName}</div>
-                <div>Rs-{el.finalPrice}</div>
-                <div>{el.size}</div>
+                <div><img src={el.img} alt="image"/></div>
+                <div><h2>{el.name}</h2></div>
+                <div><h3>{el.colorName}</h3></div>
+                <div><h3>Rs-{el.finalPrice}</h3></div>
                 
             </div>
         )
     })
 }
 </div>
-        
+      </div>  
     )
 }
