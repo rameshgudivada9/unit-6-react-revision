b3e335762fa0f92e7f46814983492eff2fbcce67
rameshgudivada9 <96101948+rameshgudivada9@users.noreply.github.com>
2022-06-06 23:07:51 +0530

    data display

--- nrupulbackend/src/App.jsx
+++ nrupulbackend/src/App.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
+import axios from "axios";
 import './App.css';
+import { Display } from './components/Display';
 
 function App() {
+  const [newdata, setNewdata] = useState([]);
+
+  async function getdata() {
+    axios
+      .get("https://yooxapi.herokuapp.com/products/men")
+      .then((res) => {
+        setNewdata(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  useEffect(()=>{
+
+   getdata()
+  },[])
+console.log(newdata[0])
   return (
     <div className="App">
-    MONGODB
+      <Display props={getdata}/>
     </div>
   );
 }
---
+++ nrupulbackend/src/components/Display.jsx
@@ -0,0 +1,19 @@
+
+
+export const Display=(props)=>{
+
+    
+
+    return(
+        <>
+    {props.newdata.map((el,i)=>
+            <div>
+<div>
+    
+</div>
+            </div>
+        
+    )}
+        </>
+    )
+}
