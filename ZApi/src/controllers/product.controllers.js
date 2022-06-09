65625d2abb47cdf6282721a0fd8faf4712e873fc
rameshgudivada9 <96101948+rameshgudivada9@users.noreply.github.com>
2022-06-09 22:07:09 +0530

    changes

--- ZApi/src/controllers/product.controllers.js
+++ ZApi/src/controllers/product.controllers.js
@@ -5,8 +5,39 @@ const router = express.Router();
 
 router.get("/", async(req,res)=>{
     try{
-        const users= await User.find({}).lean().exec();
-        return res.status(201).send(users);
+        // const users= await User.find({}).lean().exec();
+        // return res.status(201).send(users);
+
+        let filter = (req.query.category || req.query.brandName)?{$and:[]}:{};
+        if (req.query.category) {
+          
+          filter["$and"].push({ category: { $eq: req.query.category } });
+        }
+        if (req.query.brandName) {
+          
+          filter["$and"].push({ brandName: { $eq: req.query.brandName } });
+        }
+    
+        // console.log(filter["$and"]);
+        let page = req.query.page || 1;
+        let perPage = req.query.perPage || 20;
+        let skip = (page - 1) * perPage;
+        let sortData = req.query.sort
+          ? {
+            price: `${req.query.sort == "asc" ? 1 : -1}`,
+            }
+          : null;
+        // console.log(sortData, req.query.sort);
+        let totalPage = Math.ceil(
+          (await User.find().countDocuments()) / perPage
+        );
+        const product = await User.find(filter)
+          .skip(skip)
+          .limit(perPage)
+          .sort(sortData);
+    
+        return res.status(200).send({ product, totalPage });
+
     }catch(err){
         res.status(500).send({message:err.message})
     }
